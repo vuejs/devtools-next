266c9d1708fa13bccf06f058ee06406f8718968f
webfansplz <308241863@qq.com>
2023-09-09 17:01:23 +0800

    feat(browser-extension): inject overlay

--- packages/browser-extension/src/devtools.ts
+++ packages/browser-extension/src/devtools.ts
@@ -2,6 +2,23 @@
 
 const body = document.getElementsByTagName('body')[0]
 
-const iframe = document.createElement('iframe')
-iframe.src = chrome.runtime.getURL('../client/devtools-client.html')
-body.appendChild(iframe)
+// const iframe = document.createElement('iframe')
+// iframe.src = chrome.runtime.getURL('../client/devtools-client.html')
+// body.appendChild(iframe)
+
+const head = document.getElementsByTagName('head')[0]
+
+// create link stylesheet
+const link = document.createElement('link')
+link.rel = 'stylesheet'
+link.href = chrome.runtime.getURL('../overlay/style.css')
+
+// create script
+const script = document.createElement('script')
+script.src = chrome.runtime.getURL('../overlay/devtools-overlay.iife.js')
+
+// append to head
+head.appendChild(link)
+
+// append to body
+body.appendChild(script)

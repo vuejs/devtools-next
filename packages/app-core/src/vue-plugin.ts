6113139ecb791e64263cfafee7b5979ddd221096
webfansplz <308241863@qq.com>
2023-09-22 15:05:12 +0800

    feat: `useDevToolsContext` composable

--- packages/app-core/src/vue-plugin.ts
+++ packages/app-core/src/vue-plugin.ts
@@ -1,23 +1,39 @@
 import type { App, Plugin } from 'vue'
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
+import { target } from '@vue-devtools-next/shared'
 import { BridgeEvents } from '@vue-devtools-next/schema'
-import { Bridge } from './bridge'
+import type { Bridge } from './bridge'
 
 export interface DevToolsPluginOptions {
   bridge: InstanceType<typeof Bridge>
 }
 
+function initDevToolsContext(bridge: DevToolsPluginOptions['bridge']) {
+  const connected = ref(!!target.__VUE_DEVTOOLS_CTX__?.connected)
+  bridge.on(BridgeEvents.APP_CONNECTED, () => {
+    connected.value = true
+  })
+  return {
+    connected,
+  }
+}
+
 const VueDevToolsBridgeSymbol: InjectionKey<InstanceType<typeof Bridge>> = Symbol('VueDevToolsBridgeSymbol')
+const VueDevToolsContextSymbol: InjectionKey<{ connected: Ref<boolean> }> = Symbol('VueDevToolsContextSymbol')
 export function createDevToolsVuePlugin(pluginOptions: DevToolsPluginOptions): Plugin {
   return {
     install(app: App, options) {
-      Bridge.value.on(BridgeEvents.APP_CONNECTED, () => {
-      })
-      app.provide(VueDevToolsBridgeSymbol, Bridge.value)
+      const { bridge } = pluginOptions
+      app.provide(VueDevToolsBridgeSymbol, bridge)
+      app.provide(VueDevToolsContextSymbol, initDevToolsContext(bridge))
     },
   }
 }
 
 export function useDevToolsBridge() {
   return inject(VueDevToolsBridgeSymbol)!
+}
+
+export function useDevToolsContext() {
+  return inject(VueDevToolsContextSymbol)!
 }
--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-const isDark = useDark()
+import { useDevToolsContext } from '@vue-devtools-next/app-core'
+
+useDark()
+const { connected } = useDevToolsContext()
 </script>
 
 <template>
   <main class="$ui-bg-base fixed inset-0 h-screen w-screen">
-    <AppConnecting />
+    <AppConnecting v-if="!connected" />
+    <div v-else>
+      Hello DevTools
+    </div>
   </main>
 </template>

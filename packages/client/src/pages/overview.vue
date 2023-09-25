52adc0facce99a76ba15707bd5aa0dda34dc0fc7
webfansplz <308241863@qq.com>
2023-09-25 19:10:44 +0800

    chore: update

--- packages/client/src/pages/overview.vue
+++ packages/client/src/pages/overview.vue
@@ -32,75 +32,75 @@ const { componentCount } = useDevToolsContext()
           <code op40>v3.3.4</code>
         </div>
       </div>
-    </div>
 
-    <!-- Main Grid -->
-    <div flex="~ gap2 wrap">
-      <div p4 theme-card-green flex="~ col auto">
-        <div i-logos-vue text-3xl />
-        <code>v3.3.4</code>
+      <!-- Main Grid -->
+      <div flex="~ gap2 wrap">
+        <div p4 theme-card-green flex="~ col auto">
+          <div i-logos-vue text-3xl />
+          <code>v3.3.4</code>
+        </div>
+        <RouterLink flex="~ col auto" to="/pages" replace min-w-40 p4 theme-card-lime>
+          <div i-carbon-tree-view-alt text-3xl />
+          <div>100 pages</div>
+        </RouterLink>
+        <RouterLink flex="~ col auto" to="/components" replace min-w-40 p4 theme-card-lime>
+          <div i-carbon-assembly-cluster text-3xl />
+          <div>{{ componentCount }} components</div>
+        </RouterLink>
       </div>
-      <RouterLink flex="~ col auto" to="/pages" replace min-w-40 p4 theme-card-lime>
-        <div i-carbon-tree-view-alt text-3xl />
-        <div>100 pages</div>
-      </RouterLink>
-      <RouterLink flex="~ col auto" to="/components" replace min-w-40 p4 theme-card-lime>
-        <div i-carbon-assembly-cluster text-3xl />
-        <div>{{ componentCount }} components</div>
-      </RouterLink>
-    </div>
-    <div flex="~ gap-6 wrap" mt-5 items-center justify-center>
-      <a href="https://github.com/webfansplz/vue-devtools-next" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-blue" transition>
-        <div i-carbon-star />
-        Star on GitHub
-      </a>
-      <a href="https://github.com/webfansplz/vue-devtools-next/discussions/1" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-yellow" transition>
-        <div i-carbon-data-enrichment />
-        Ideas & Suggestions
-      </a>
-      <a href="https://github.com/webfansplz/vue-devtools-next/discussions/2" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-lime" transition>
-        <div i-carbon-plan />
-        Project Roadmap
-      </a>
-      <a href="https://github.com/webfansplz/vue-devtools-next/issues" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-rose" transition>
-        <div i-carbon-debug />
-        Bug Reports
-      </a>
-      <RouterLink to="/settings" flex="~ gap1" replace inline-block items-center op50 hover:op80>
-        <div i-carbon-settings />
-        Settings
-      </RouterLink>
-    </div>
-    <div flex-auto />
-    <div flex="~ gap-1" cursor-default items-center justify-center pb-8 text-sm op40>
-      Press
-      <template v-if="isMacOS()">
-        <VueButton>
-          ⇧ Shift
-        </VueButton>
-        <span>+</span>
-        <VueButton>
-          ⌥ Option
-        </VueButton>
-        <span>+</span>
-        <VueButton>
-          D
-        </VueButton>
-      </template>
-      <template v-else>
-        <VueButton>
-          Shift
-        </VueButton>
-        <span>+</span>
-        <VueButton>
-          Alt
-        </VueButton>
-        <span>+</span>
-        <VueButton>
-          D
-        </VueButton>
-      </template>
-      to toggle DevTools
+      <div flex="~ gap-6 wrap" mt-5 items-center justify-center>
+        <a href="https://github.com/webfansplz/vue-devtools-next" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-blue" transition>
+          <div i-carbon-star />
+          Star on GitHub
+        </a>
+        <a href="https://github.com/webfansplz/vue-devtools-next/discussions/1" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-yellow" transition>
+          <div i-carbon-data-enrichment />
+          Ideas & Suggestions
+        </a>
+        <a href="https://github.com/webfansplz/vue-devtools-next/discussions/2" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-lime" transition>
+          <div i-carbon-plan />
+          Project Roadmap
+        </a>
+        <a href="https://github.com/webfansplz/vue-devtools-next/issues" target="_blank" flex="~ gap1" items-center op50 hover="op100 text-rose" transition>
+          <div i-carbon-debug />
+          Bug Reports
+        </a>
+        <RouterLink to="/settings" flex="~ gap1" replace inline-block items-center op50 hover:op80>
+          <div i-carbon-settings />
+          Settings
+        </RouterLink>
+      </div>
+      <div flex-auto />
+      <div flex="~ gap-1" cursor-default items-center justify-center pb-8 text-sm op40>
+        Press
+        <template v-if="isMacOS()">
+          <VueButton>
+            ⇧ Shift
+          </VueButton>
+          <span>+</span>
+          <VueButton>
+            ⌥ Option
+          </VueButton>
+          <span>+</span>
+          <VueButton>
+            D
+          </VueButton>
+        </template>
+        <template v-else>
+          <VueButton>
+            Shift
+          </VueButton>
+          <span>+</span>
+          <VueButton>
+            Alt
+          </VueButton>
+          <span>+</span>
+          <VueButton>
+            D
+          </VueButton>
+        </template>
+        to toggle DevTools
+      </div>
     </div>
   </PanelGrids>
 </template>

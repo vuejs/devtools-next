1ca27255a76eef39b0fbf2d9729a41862276bbf7
webfansplz <308241863@qq.com>
2023-10-06 17:53:15 +0800

    fix: tree-node has children

--- packages/client/src/components/components/ComponentTreeNode.vue
+++ packages/client/src/components/components/ComponentTreeNode.vue
@@ -29,14 +29,14 @@ function select() {
     @click="select"
   >
     <!-- expand-icon -->
-    <ExpandIcon v-if="data.children" :value="isExpanded" group-hover:text-white class="[.active_&]:text-white" @click.prevent.stop="toggleExpanded" />
+    <ExpandIcon v-if="data.children?.length" :value="isExpanded" group-hover:text-white class="[.active_&]:text-white" @click.prevent.stop="toggleExpanded" />
     <i v-else inline-block h-6 w-6 />
     <!-- component name -->
     <span text-primary-400 group-hover:text-white class="[.active_&]:text-white">
       {{ data.name }}
     </span>
   </div>
-  <template v-if="data.children && isExpanded">
+  <template v-if="data.children?.length && isExpanded">
     <ComponentTreeNode v-for="(item, index) in data.children" :key="index" :data="item" :depth="depth + 1" />
   </template>
 </template>

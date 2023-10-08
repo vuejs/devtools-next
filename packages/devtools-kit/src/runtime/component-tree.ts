24db356b277ffa6697b12e3004673e3e9caa264d
webfansplz <308241863@qq.com>
2023-10-08 19:15:21 +0800

    feat: component-tree update logic

--- packages/devtools-kit/src/runtime/component-tree.ts
+++ packages/devtools-kit/src/runtime/component-tree.ts
@@ -57,4 +57,60 @@ export function initComponentTree() {
 
     callBuffer(DevToolsEvents.COMPONENT_TREE_UPDATED, treeNode!)
   })
+
+  api.on.componentUpdated(async (app, uid, parentUid, component) => {
+    if (app?._instance?.type?.devtools?.hide)
+      return
+
+    if (!app || (typeof uid !== 'number' && !uid) || !component)
+      return
+
+    const id = await getComponentId({
+      app, uid, instance: component,
+    }) as string
+    const appRecord = await getAppRecord(app)
+
+    if (component) {
+      if (component.__VUE_DEVTOOLS_UID__ == null)
+        component.__VUE_DEVTOOLS_UID__ = id
+
+      if (!appRecord?.instanceMap.has(id))
+        appRecord?.instanceMap.set(id, component)
+    }
+
+    if (!appRecord)
+      return
+
+    const treeNode = await getComponentTree({
+      appRecord,
+      recursively: false,
+    })
+
+    callBuffer(DevToolsEvents.COMPONENT_TREE_UPDATED, treeNode!)
+  })
+
+  api.on.componentRemoved(async (app, uid, parentUid, component) => {
+    if (app?._instance?.type?.devtools?.hide)
+      return
+
+    if (!app || (typeof uid !== 'number' && !uid) || !component)
+      return
+
+    const appRecord = await getAppRecord(app)
+
+    if (!appRecord)
+      return
+
+    const id = await getComponentId({
+      app, uid, instance: component,
+    }) as string
+    appRecord?.instanceMap.delete(id)
+
+    const treeNode = await getComponentTree({
+      appRecord,
+      recursively: false,
+    })
+
+    callBuffer(DevToolsEvents.COMPONENT_TREE_UPDATED, treeNode!)
+  })
 }

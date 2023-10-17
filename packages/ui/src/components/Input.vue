7ea8b4a161d49e71e81d1cc1480fa915c7454b05
alexzhang1030 <1642114555@qq.com>
2023-10-17 12:23:31 +0800

    feat: Input

--- packages/ui/src/components/Input.vue
+++ packages/ui/src/components/Input.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { useVModel } from '@vueuse/core'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import VueIcon from './Icon.vue'
+import VueLoading from './LoadingIndicator.vue'
 
 const props = withDefaults(defineProps<{
   modelValue: string
-  type?: 'input' | 'textarea' | 'number'
   placeholder?: string
-  round?: boolean
+  variant?: 'normal' | 'accent' | 'flat'
+  disabled?: boolean
+  password?: boolean
+  leftIcon?: string
+  rightIcon?: string
+  loading?: boolean
 }>(), {
-  type: 'input',
   placeholder: '',
-  round: false,
+  variant: 'normal',
+  disabled: false,
+  password: false,
+  /**
+   * loading will auto enable disabled
+   */
+  loading: false,
 })
 
 const emit = defineEmits<{
@@ -21,21 +32,77 @@ const emit = defineEmits<{
 const focused = ref(false)
 
 const value = useVModel(props, 'modelValue', emit)
+
+const noFocusAnimation = computed(() => props.variant === 'flat')
+
+const disabled = computed(() => props.disabled || props.loading)
 </script>
 
 <template>
-  <div class="relative min-w-200px">
-    <component
-      :is="props.type === 'textarea' ? 'textarea' : 'input'"
-      v-model="value" class="$ui-base w-full outline-none border-primary-100 b-1 rounded-sm"
-      v-bind="$attrs" @keydown.tab.prevent="emit('keyTab', $event)"
-      @focus="focused = true" @blur="focused = false"
-    />
+  <div
+    class="container" :class="{
+      'border-none bg-transparent': variant === 'flat',
+      'cursor-not-allowed opacity-50': disabled,
+      'accent': variant === 'accent',
+      'focused': focused,
+    }"
+    @click="focused = true"
+  >
+    <div v-if="leftIcon" class="icon">
+      <VueIcon :icon="leftIcon" />
+    </div>
+    <input
+      v-model="value"
+      class="input" v-bind="$attrs" :type="props.password ? 'password' : 'text'"
+      placeholder="placeholder" :disabled="disabled"
+      @blur="focused = false"
+    >
+    <div v-if="loading" class="icon">
+      <VueLoading />
+    </div>
+    <div v-else-if="rightIcon" class="icon">
+      <VueIcon :icon="rightIcon" />
+    </div>
     <!-- Focus animation -->
     <div
-      v-if="!round" class="bg-primary-500 absolute z-9999 bottom--1px left-0 h-2px pointer-events-none transition-all duration-240" :class="[
-        focused ? 'left-0 right-0 opacity-100' : 'left-50% right-50% opacity-0',
-      ]"
+      v-if="!noFocusAnimation" class="animation"
     />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  @apply relative w-auto min-w-200px overflow-hidden b-1 border-primary-100
+         flex justify-between items-center gap-2px py-5px px12px color-gray-800 dark:color-gray-100;
+  .input {
+    @apply $ui-base w-full outline-none rounded-sm bg-white color-inherit
+        placeholder-color-gray-500 dark:placeholder-gray-300;
+  }
+  .icon {
+    @apply transition-colors $ui-fcc color-gray-500 dark:color-gray-300;
+  }
+  .animation {
+    @apply absolute z-9999 bottom--1px bg-primary-500
+           h-3px pointer-events-none transition-all duration-240
+           left-50% right-50% opacity-0;
+  }
+  &.accent {
+    &.focused {
+      .icon {
+        @apply color-accent-500;
+      }
+    }
+    .animation {
+      @apply bg-accent-500;
+    }
+  }
+  &.focused {
+    .icon {
+      @apply color-primary-500;
+    }
+    .animation {
+      @apply left-0 right-0 opacity-100;
+    }
+  }
+}
+</style>

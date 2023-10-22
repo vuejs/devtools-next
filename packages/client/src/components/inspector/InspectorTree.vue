7daf81aeb7e8be0438e561a8dcfd9726ecf379a0
webfansplz <308241863@qq.com>
2023-10-22 22:33:21 +0800

    feat: add inspector tree node tags

--- packages/client/src/components/inspector/InspectorTree.vue
+++ packages/client/src/components/inspector/InspectorTree.vue
@@ -3,6 +3,12 @@ const props = defineProps<{
   data: {
     label: string
     id: string
+    tags: {
+      label: string
+      textColor: number
+      backgroundColor: number
+      tooltip: string
+    }[]
   }
 }>()
 
@@ -18,6 +24,21 @@ const { isSelected, toggleSelected } = useSelect('inspector-tree', props.data.id
   <div class="selectable-item" :class="{ active: isSelected }" @click="toggleSelected(data.id)">
     <span>
       {{ data.label }}
+    </span>
+    <span
+      v-for="(tag, index) of data.tags"
+      :key="index"
+      v-tooltip="{
+        content: tag.tooltip,
+        html: true,
+      }"
+      :style="{
+        color: `#${tag.textColor.toString(16).padStart(6, '0')}`,
+        backgroundColor: `#${tag.backgroundColor.toString(16).padStart(6, '0')}`,
+      }"
+      class="text-[0.75rem] px-1 rounded-sm ml-2 leading-snug"
+    >
+      {{ tag.label }}
     </span>
   </div>
 </template>

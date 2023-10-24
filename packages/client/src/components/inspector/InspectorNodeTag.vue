57fe52c55bddbf29b660b1cbe116041907f448bf
webfansplz <308241863@qq.com>
2023-10-24 19:12:56 +0800

    feat: color util

--- packages/client/src/components/inspector/InspectorNodeTag.vue
+++ packages/client/src/components/inspector/InspectorNodeTag.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { InspectorNodeTag } from 'vue-devtools-kit'
+import { toHex } from '~/utils'
 
 defineProps<{
   tag: InspectorNodeTag
@@ -13,8 +14,8 @@ defineProps<{
       html: true,
     }"
     :style="{
-      color: `#${tag.textColor.toString(16).padStart(6, '0')}`,
-      backgroundColor: `#${tag.backgroundColor.toString(16).padStart(6, '0')}`,
+      color: `#${toHex(tag.textColor)}`,
+      backgroundColor: `#${toHex(tag.backgroundColor)}`,
     }"
     class="text-[0.75rem] px-1 rounded-sm ml-2 leading-snug"
   >

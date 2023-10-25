e7e19c509695d227d97d0d4be66d924863243374
alexzhang1030 <1642114555@qq.com>
2023-10-25 09:58:58 +0800

    fix: toHex will emit #xxxxxx, should cut the # symbol

--- packages/client/src/utils/color.ts
+++ packages/client/src/utils/color.ts
@@ -12,7 +12,7 @@ export function dimColor(color: number, isDark: boolean, amount = 20) {
   else
     c = c.lighten(amount)
 
-  return Number.parseInt(`0x${c.toHex()}`)
+  return Number.parseInt(`0x${c.toHex().slice(1)}`)
 }
 
 export function boostColor(color: number, isDark: boolean, amount = 10) {
@@ -23,5 +23,5 @@ export function boostColor(color: number, isDark: boolean, amount = 10) {
   else
     c = c.darken(amount)
 
-  return Number.parseInt(`0x${c.toHex()}`)
+  return Number.parseInt(`0x${c.toHex().slice(1)}`)
 }

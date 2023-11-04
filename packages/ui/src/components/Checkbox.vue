5be3a5ba0e635f9880f01e1a1a0e5ea8a4cd9262
alexzhang1030 <1642114555@qq.com>
2023-11-04 17:34:41 +0800

    feat(ui): add checkbox component

---
+++ packages/ui/src/components/Checkbox.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+
+const props = withDefaults(defineProps<{
+  modelValue: boolean
+  variant?: 'normal' | 'accent'
+}>(), {
+  variant: 'normal',
+})
+
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean]
+}>()
+
+const value = useVModel(props, 'modelValue', emit)
+</script>
+
+<template>
+  <div
+    class="$ui-base relative cursor-pointer
+    w20px h20px w-full h-full border-2px rounded-5px
+    transition-colors duration-200 ease-in-out $ui-inline-fcc
+    "
+    :class="[
+      variant === 'normal'
+        ? 'border-primary-500'
+        : 'border-accent-500',
+      value
+        ? variant === 'normal'
+          ? 'bg-primary-100 dark:bg-primary-200'
+          : 'bg-accent-100 dark:bg-accent-200'
+        : '',
+    ]"
+    @click="value = !value"
+  >
+    <div
+      :class="[
+        value ? 'opacity-100' : 'opacity-0',
+      ]"
+      class="w-35% h-60%
+      border-solid border-width-[0_2px_2px_0] border-black transition-opacity duration-200 ease-in-out
+      transform-rotate-45 transform-translate-y--1px
+    "
+    />
+  </div>
+</template>
---
+++ packages/ui/storybook/Checkbox.story.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Checkbox from '../src/components/Checkbox.vue'
+
+const checked = ref(false)
+</script>
+
+<template>
+  <Story>
+    <Variant title="normal">
+      <div>
+        <span>value: {{ checked }}</span>
+        <Checkbox v-model="checked" />
+      </div>
+    </Variant>
+    <Variant title="accent">
+      <div>
+        value: {{ checked }} <Checkbox v-model="checked" variant="accent" />
+      </div>
+    </Variant>
+  </Story>
+</template>

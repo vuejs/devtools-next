0723dcb76752aa8bdfec1f059e38390eede79b2c
Alex <49969959+alexzhang1030@users.noreply.github.com>
2023-11-05 06:07:03 -0600

    fix: parse overlay safeArea safely(#11)

--- packages/overlay/src/utils/index.ts
+++ packages/overlay/src/utils/index.ts
@@ -3,3 +3,9 @@ export function clamp(value: number, min: number, max: number) {
 }
 
 export const checkIsSafari = () => navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')
+
+export function pixelToNumber(value: string | number) {
+  if (typeof value === 'string')
+    return value.endsWith('px') ? +value.slice(0, -2) : +value
+  return value
+}

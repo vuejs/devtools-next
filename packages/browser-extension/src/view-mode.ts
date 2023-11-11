37e46b62e145aa9aee1bcf0a66add0a766fab655
webfansplz <308241863@qq.com>
2023-11-11 21:26:38 +0800

    chore: update

--- packages/browser-extension/src/view-mode.ts
+++ packages/browser-extension/src/view-mode.ts
@@ -3,7 +3,7 @@ window.addEventListener('message', onMessage)
 function onMessage(e) {
   const payload = e.data.payload
   if (e.data.source === '__VUE_DEVTOOLS_PROXY__' && payload.event === 'toggle-view-mode') {
-    window.removeEventListener('message', onMessage)
+    // window.removeEventListener('message', onMessage)
     window.postMessage({
       event: 'toggle-view-mode',
       data: payload.data,

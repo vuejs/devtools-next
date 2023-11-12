1281be85d33ac903ffe71081c84a21bb919831fc
webfansplz <308241863@qq.com>
2023-11-12 21:48:54 +0800

    chore: update

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import type { Ref } from 'vue'
 import { useDevToolsState } from '@vue-devtools-next/core'
+import { isInChromePanel } from '@vue-devtools-next/shared'
 
 // @TODO: fix browser extension cross-origin localStorage issue
 useColorMode()
 const router = useRouter()
 const { connected } = useDevToolsState()
 
+const viewMode = inject<Ref<'overlay' | 'panel'>>('viewMode', ref('overlay'))
+const viewModeSwitchVisible = computed(() => viewMode.value === 'overlay' && isInChromePanel)
+const { toggle } = useToggleViewMode()
 watch(connected, (v) => {
   if (v)
     router.replace('/components')
@@ -17,6 +22,7 @@ watch(connected, (v) => {
 <template>
   <main class="$ui-bg-base fixed inset-0 h-screen w-screen">
     <AppConnecting v-if="!connected" />
+    <ViewModeSwitch v-else-if="viewModeSwitchVisible" />
     <div v-else class="h-full of-auto">
       <RouterView />
     </div>

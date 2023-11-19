59a2b196560c55aaba6460c29ca422b94b532623
webfansplz <308241863@qq.com>
2023-11-19 20:39:53 +0800

    chore: fix types

--- packages/client/src/composables/state.ts
+++ packages/client/src/composables/state.ts
@@ -1,4 +1,9 @@
-export function useDevToolsClientState() {
+import type { RemovableRef } from '@vueuse/core'
+
+export function useDevToolsClientState(): RemovableRef<{
+  isFirstVisit: boolean
+  route: string
+}> {
   const state = useLocalStorage('__VUE_DEVTOOLS_CLIENT_STATE__', {
     isFirstVisit: true,
     route: '/',

2d9fe268673b5a85aaabd7b678e1fdc72173ca85
webfansplz <308241863@qq.com>
2023-11-19 20:31:04 +0800

    chore: update

--- packages/client/src/composables/state.ts
+++ packages/client/src/composables/state.ts
@@ -1,8 +1,8 @@
 export function useDevToolsClientState() {
-  const localState = useLocalStorage('__VUE_DEVTOOLS_CLIENT_STATE__', {
+  const state = useLocalStorage('__VUE_DEVTOOLS_CLIENT_STATE__', {
     isFirstVisit: true,
     route: '/',
   })
 
-  return localState
+  return state
 }

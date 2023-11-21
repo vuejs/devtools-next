311a032387a753bcdfe0077b044bf78182b81c85
alexzhang1030 <1642114555@qq.com>
2023-11-21 18:08:35 +0800

    feat(client/graph): render legends

---
+++ packages/client/src/composables/graph.ts
@@ -0,0 +1,39 @@
+export const legends = {
+  vue: {
+    color: '#42b883',
+  },
+  ts: {
+    color: '#3B86CB',
+  },
+  js: {
+    color: '#d6cb2d',
+  },
+  json: {
+    color: '#cf8f30',
+  },
+  css: {
+    color: '#e6659a',
+  },
+  html: {
+    color: '#e34c26',
+  },
+  svelte: {
+    color: '#ff3e00',
+  },
+  jsx: {
+    color: '#54B9D1',
+  },
+  tsx: {
+    color: '#4FC7FF',
+  },
+} satisfies Record<string, { color: string }>
+
+export function useLegends() {
+  const [legendShow, toggleLegend] = useToggle(false)
+
+  return {
+    legendsData: Object.entries(legends),
+    legendShow,
+    toggleLegend,
+  }
+}
--- packages/client/src/pages/graph.vue
+++ packages/client/src/pages/graph.vue
@@ -8,10 +8,42 @@ onDevToolsClientConnected(() => {
     console.log('res', res)
   })
 })
+
+const { legendsData, legendShow, toggleLegend } = useLegends()
 </script>
 
 <template>
-  <div>
+  <div relative w-full h-full>
     Graph
+
+    <!-- legends -->
+    <div
+      absolute bottom-0 px4 py2 rounded-tr-lg w-260px grid gap-2 grid-cols-3 bg-gray="100/40"
+      transition-transform duration-300 ease-in-out
+      :class="[
+        legendShow ? 'translate-y-0' : 'translate-y-full',
+      ]"
+    >
+      <div
+        absolute top--8 px4 py1 bg-primary-500 text-white rounded-tr-lg cursor-pointer class="group"
+        @click="() => toggleLegend()"
+      >
+        Legend <div
+          :class="[
+            legendShow
+              ? 'i-material-symbols-arrow-downward-alt group-hover:translate-y-10%'
+              : 'i-material-symbols:arrow-upward-alt-rounded group-hover:translate-y--10%',
+          ]" transition-transform
+        />
+      </div>
+      <div v-for="item of Object.entries(legendsData)" :key="item[1][0]" flex gap-2 items-center>
+        <div
+          rounded-full w12px h12px :style="{
+            backgroundColor: item[1][1].color,
+          }"
+        />
+        <span>{{ item[1][0] }}</span>
+      </div>
+    </div>
   </div>
 </template>

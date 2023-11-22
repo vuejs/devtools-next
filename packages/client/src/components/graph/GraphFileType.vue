1f12f1d6514619ce2aeda025018d70df18aa3c94
alexzhang1030 <1642114555@qq.com>
2023-11-22 17:58:53 +0800

    ui: update graph file type

--- packages/client/src/components/graph/GraphFileType.vue
+++ packages/client/src/components/graph/GraphFileType.vue
@@ -6,20 +6,26 @@ const { fileTypeData, fileTypeShow, toggleFileType } = useFileTypes()
   <div
     absolute bottom-0 px4 py2 rounded-tr-lg w-260px grid gap-2 grid-cols-3 bg-base b-1 border-base
     transition-transform duration-300
-    :class="[
-      fileTypeShow ? 'translate-y-0' : 'translate-y-full',
-    ]"
+    class="group"
+    :class="{
+      'translate-x-[calc(-100%+30px)] translate-y-[calc(100%-30px)]': !fileTypeShow,
+    }"
   >
     <div
-      absolute top-0 translate-y="-100%" px4 py1 bg-primary-500 text-white rounded-tr-lg cursor-pointer class="group"
-      transition-opacity @click="() => toggleFileType()"
+      absolute right-0 bg-primary-500 text-white rounded-tr-lg cursor-pointer class="group"
+      transition-opacity w30px h30px flex="~ justify-center items-center"
+      :class="{
+        'rounded-bl-lg opacity-0 group-hover:opacity-100': fileTypeShow,
+      }"
+      @click="() => toggleFileType()"
     >
-      File Type <div
+      <div
+        transition-transform
         :class="[
           fileTypeShow
-            ? 'group-hover:translate-y-10% rotate-180'
-            : 'group-hover:translate-y--10%',
-        ]" transition-transform i-material-symbols:arrow-upward-alt-rounded
+            ? 'i-material-symbols-arrow-downward-alt rotate-45 hover:(translate-x--10% translate-y-10%)'
+            : 'i-mdi-file-outline',
+        ]"
       />
     </div>
     <div v-for="item of fileTypeData" :key="item.key" flex gap-2 items-center>

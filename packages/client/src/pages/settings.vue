400d7ba43af67f65634b489e1cae56b73ca4037d
alexzhang1030 <1642114555@qq.com>
2023-11-25 12:36:45 +0800

    chore: setup tabs settings

---
+++ packages/client/src/pages/settings.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { VueCard, VueSwitch } from '@vue-devtools-next/ui'
+
+// TODO: plugin tabs
+const categories = builtinTab
+
+const hiddenTabCategories = ref<string[]>([''])
+const hiddenTabs = ref<string[]>([])
+const pinnedTabs = ref<string[]>([])
+
+function toggleTab(name: string, v: boolean) {
+  if (v)
+    hiddenTabs.value = hiddenTabs.value.filter(i => i !== name)
+  else
+    hiddenTabs.value.push(name)
+}
+
+function toggleTabCategory(name: string, v: boolean) {
+  if (v)
+    hiddenTabCategories.value = hiddenTabCategories.value.filter(i => i !== name)
+  else
+    hiddenTabCategories.value.push(name)
+}
+
+function togglePinTab(name: string) {
+  if (pinnedTabs.value.includes(name))
+    pinnedTabs.value = pinnedTabs.value.filter(i => i !== name)
+  else
+    pinnedTabs.value.push(name)
+}
+
+function pinMove(name: string, delta: number) {
+  const index = pinnedTabs.value.indexOf(name)
+  if (index === -1)
+    return
+
+  const newIndex = index + delta
+  if (newIndex < 0 || newIndex >= pinnedTabs.value.length)
+    return
+
+  const newPinnedTabs = [...pinnedTabs.value]
+  newPinnedTabs.splice(index, 1)
+  newPinnedTabs.splice(newIndex, 0, name)
+  pinnedTabs.value = newPinnedTabs
+}
+</script>
+
+<template>
+  <div px8 py6 of-auto w-full h-full>
+    <IconTitle
+      class="mb-5 text-xl op75"
+      icon="i-carbon-settings-adjust"
+      text="DevTools Settings"
+    />
+    <div grid="~ md:cols-2 gap-x-10 gap-y-3" max-w-300>
+      <div flex="~ col gap-2">
+        <h3 text-lg>
+          Tabs
+        </h3>
+        <template v-for="[name, tabs] of categories" :key="name">
+          <VueCard
+            v-if="tabs.length" p3 flex="~ col gap-1"
+            :class="hiddenTabCategories.includes(name) ? 'op50 grayscale' : ''"
+          >
+            <VueSwitch
+              :model-value="!hiddenTabCategories.includes(name)"
+              class="flex row-reverse py1 pl2 pr1"
+              @update:model-value="(v: boolean) => toggleTabCategory(name, v)"
+            >
+              <div flex="~ gap-2" flex-auto items-center justify-start>
+                <span capitalize op75>{{ name }}</span>
+              </div>
+            </VueSwitch>
+
+            <div mx--1 my1 h-1px border="b base" op75 />
+
+            <template v-for="tab of tabs" :key="tab.name">
+              <VueSwitch
+                class="flex row-reverse py1 pl2 pr1 n-primary"
+                :model-value="!hiddenTabs.includes(tab.name)"
+                :class="hiddenTabs.includes(tab.name) ? 'op35' : ''"
+                @update:model-value="(v: boolean) => toggleTab(tab.name, v)"
+              >
+                <div flex="~ gap-2" text-sm flex-auto items-center justify-start pr-4>
+                  <TabIcon text-xl :icon="tab.icon" :title="tab.title" />
+                  <span>{{ tab.title }}</span>
+                  <div flex-auto />
+                  <template v-if="pinnedTabs.includes(tab.name)">
+                    <NButton
+                      icon="i-carbon-caret-up"
+                      :disabled="pinnedTabs.indexOf(tab.name) === 0"
+                      :border="false"
+                      @click="pinMove(tab.name, -1)"
+                    />
+                    <NButton
+                      icon="i-carbon-caret-down"
+                      :disabled="pinnedTabs.indexOf(tab.name) === pinnedTabs.length - 1"
+                      :border="false"
+                      @click="pinMove(tab.name, 1)"
+                    />
+                  </template>
+                  <button class="px1 py1 text-sm hover:bg-active flex items-center" @click.stop>
+                    <div :class="pinnedTabs.includes(tab.name) ? ' i-carbon-pin-filled rotate--45' : ' i-carbon-pin op45'" />
+                  </button>
+                </div>
+              </VueSwitch>
+            </template>
+          </VueCard>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>

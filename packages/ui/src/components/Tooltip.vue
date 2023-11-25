afc1c41518c07d97c092860565a607c6445dd90c
alexzhang1030 <1642114555@qq.com>
2023-11-25 10:20:11 +0800

    chore: improve ui

---
+++ packages/ui/src/components/Tooltip.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Placement } from 'floating-vue'
+import { Tooltip } from 'floating-vue'
+
+defineProps<{
+  placement?: Placement
+  disabled?: boolean
+}>()
+</script>
+
+<template>
+  <Tooltip
+    :disabled="disabled"
+    :placement="placement"
+    v-bind="$attrs"
+  >
+    <slot />
+    <template #popper>
+      <slot name="popper" />
+    </template>
+  </Tooltip>
+</template>
---
+++ packages/ui/storybook/Tooltip.story.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { VueButton } from '../src'
+import VueTooltip from '../src/components/Tooltip.vue'
+</script>
+
+<template>
+  <Story>
+    <div class="flex">
+      <VueTooltip>
+        <VueButton>Hover me</VueButton>
+        <template #popper>
+          <div class="p-4">
+            Tooltip content
+          </div>
+        </template>
+      </VueTooltip>
+    </div>
+  </Story>
+</template>

2a0910034d278191e908edcd02fde541b6826752
webfansplz <308241863@qq.com>
2023-12-08 19:22:11 +0800

    chore: update

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -2,7 +2,6 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
-import { connect } from 'vue-devtools-next'
 
 // import App from './App.vue'
 import App from './App.preview.vue'
@@ -12,7 +11,7 @@ import Hey from './pages/Hey.vue'
 import './style.css'
 
 // connect to remote devtools
-connect('http://localhost', 8098)
+// connect('http://localhost', 8098)
 
 const pinia = createPinia()
 

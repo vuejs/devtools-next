d4c4d7b1aa3003fa20b6ce363fe88893c5e10882
Alex <49969959+alexzhang1030@users.noreply.github.com>
2023-12-17 20:08:51 +0800

    fix(ui): Overlay component position (#79)

--- packages/ui/src/components/Overlay.vue
+++ packages/ui/src/components/Overlay.vue
@@ -9,7 +9,7 @@ defineProps<OverlayProps>()
 
 <template>
   <div
-    class="absolute absolute left-0 top-0 $ui-z-max h-full w-full $ui-fcc transition-opacity transition-duration-300 $ui-bg-base"
+    class="fixed left-0 top-0 $ui-z-max h-full w-full $ui-fcc transition-opacity transition-duration-300 $ui-bg-base"
     :class="[
       dim ? 'bg-opacity-50!' : 'bg-opacity-0!',
       blur ? 'backdrop-blur-sm' : '',

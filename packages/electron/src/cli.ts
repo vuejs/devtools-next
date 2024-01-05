877fe9bf2f3cc6d9e1fcedcee21456cfbfdcac83
alexzhang1030 <1642114555@qq.com>
2024-01-05 15:56:47 +0800

    fix: path

--- packages/electron/src/cli.ts
+++ packages/electron/src/cli.ts
@@ -1,7 +1,8 @@
 import { execaSync } from 'execa'
 import electron from 'electron'
+import { resolve } from 'pathe'
 
-const appPath = new URL('../dist/app.cjs', import.meta.url).pathname
+const appPath = resolve(new URL('../dist/app.cjs', import.meta.url).pathname)
 const argv = process.argv.slice(2)
 
 const result = execaSync(electron as unknown as string, [appPath].concat(argv), {

acb28c35561f7b3ff0cee95b34512f812ae7b602
arlo <webfansplz@gmail.com>
2024-01-08 12:46:56 +0800

    chore: update

--- packages/ui/storybook/DarkToggle.story.vue
+++ packages/ui/storybook/DarkToggle.story.vue
@@ -24,7 +24,7 @@ const dark = computed({
               toggle(e)
             }"
           >
-            <Icon :class="isDark.value ? 'i-carbon-moon' : 'i-carbon-sun'" />
+            <Icon :class="isDark ? 'i-carbon-moon' : 'i-carbon-sun'" />
           </Button>
         </template>
       </DarkToggle>

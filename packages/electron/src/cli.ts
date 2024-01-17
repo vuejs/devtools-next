922b77c2152beb45163a136640edf97249284014
alexzhang1030 <1642114555@qq.com>
2024-01-17 12:07:22 +0800

    fix(electron): should not encode the app file path

--- packages/electron/src/cli.ts
+++ packages/electron/src/cli.ts
@@ -2,7 +2,7 @@ import { execaSync } from 'execa'
 import electron from 'electron'
 import { resolve } from 'pathe'
 
-const appPath = resolve(new URL('../dist/app.cjs', import.meta.url).pathname)
+const appPath = decodeURIComponent(resolve(new URL('../dist/app.cjs', import.meta.url).pathname))
 const argv = process.argv.slice(2)
 
 const result = execaSync(electron as unknown as string, [appPath].concat(argv), {

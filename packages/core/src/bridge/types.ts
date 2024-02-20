ee3e6459d82518c688be7ebdd9110487e81b5442
Fan Pei <fanpei920@gmail.com>
2024-02-21 01:07:39 +0900

    feat: Adapt old devtools' component update API

--- packages/core/src/bridge/types.ts
+++ packages/core/src/bridge/types.ts
@@ -16,6 +16,8 @@ export enum BridgeEvents {
   SEND_INSPECTOR_TREE = 'inspector-tree:send',
   // inspector state
   SEND_INSPECTOR_STATE = 'inspector-state:send',
+  // component updated
+  COMPONENT_UPDATED = 'component:updated',
 
   // edit related things (components/pinia/route, etc...) they are all fired the same event
   // so that we can decouple the specific logic to the `devtools-kit` package.

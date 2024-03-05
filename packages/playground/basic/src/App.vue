c54090b04784f405e38f478018d2d948dcb9626d
Lo <l709937065@gmail.com>
2024-03-05 09:17:54 +0800

    chore: remove verbose DarkToggle in playground/basic

--- packages/playground/basic/src/App.vue
+++ packages/playground/basic/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import DarkToggle from './components/DarkToggle.vue'
 </script>
 
 <template>
@@ -26,9 +25,6 @@ import DarkToggle from './components/DarkToggle.vue'
           /hey/123123
         </span>
       </RouterLink>
-    </div>
-    <div text-center>
-      <DarkToggle />
     </div>
   </div>
 </template>

e049e34535adc3b089b1105a38fa7dbc5758ee86
Fan Pei <fanpei920@gmail.com>
2024-03-06 17:03:50 +0900

    chore: Add test for setting Map/Set

--- packages/devtools-kit/__tests__/component/editor.test.ts
+++ packages/devtools-kit/__tests__/component/editor.test.ts
@@ -3,8 +3,10 @@ import { RefStateEditor } from '../../src/core/component/state/editor'
 describe('editor: RefStateEditor', () => {
   const editor = new RefStateEditor()
 
+  // Plain object
   // eslint-disable-next-line test/consistent-test-it
   test.each([
+    // Plain object.
     // Add new key.
     { refValue: { foo: 'bar' }, newValue: { foo: 'bar', bar: 'baz' } },
     // Add new key and modify origin value.
@@ -20,5 +22,36 @@ describe('editor: RefStateEditor', () => {
   ])('$refValue can be modified to $newValue by RefStateEditor.set', ({ refValue, newValue }) => {
     editor.set(refValue as any, newValue)
     expect(refValue).toEqual(newValue)
+  })
+
+  // Native set
+  it('refStateEditor.set on native set', () => {
+    const refValue = new Set(['foo', 'bar'])
+    const newValue = ['baz', 'foo', 'bar']
+    const targetValue = new Set(['baz', 'foo', 'bar'])
+    editor.set(refValue as any, newValue)
+    expect(refValue).toEqual(targetValue)
+  })
+
+  // Native map
+  // eslint-disable-next-line test/consistent-test-it
+  test.each([
+    // Add new key.
+    { refValue: new Map([['foo', 'bar']]), newValue: { foo: 'bar', bar: 'baz' }, targetValue: new Map([['foo', 'bar'], ['bar', 'baz']]) },
+    // Add new key and modify origin value.
+    { refValue: new Map([['foo', 'bar']]), newValue: { foo: 'barr', bar: 'baz' }, targetValue: new Map([['foo', 'barr'], ['bar', 'baz']]) },
+    // Add new key and modify origin value.
+    { refValue: new Map([['foo', 'bar']]), newValue: { foo: 'barr', bar: 'baz' }, targetValue: new Map([['foo', 'barr'], ['bar', 'baz']]) },
+    // Modify origin value.
+    { refValue: new Map([['foo', 'bar']]), newValue: { foo: 'barr' }, targetValue: new Map([['foo', 'barr']]) },
+    // Remove key.
+    { refValue: new Map([['foo', 'bar']]), newValue: {}, targetValue: new Map() },
+    // Remove key and modify origin value.
+    { refValue: new Map([['foo', 'bar']]), newValue: { foo: 'barr' }, targetValue: new Map([['foo', 'barr']]) },
+    // Remove key and add new key.
+    { refValue: new Map([['foo', 'bar']]), newValue: { bar: 'baz' }, targetValue: new Map([['bar', 'baz']]) },
+  ])('%o can be modified to $newValue by RefStateEditor.set', ({ refValue, newValue, targetValue }) => {
+    editor.set(refValue as any, newValue)
+    expect(refValue).toEqual(targetValue || newValue)
   })
 })

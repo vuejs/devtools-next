bb57ec268b7b6bb6ea691cf6d39e06d3488a07e4
Arlo <webfansplz@gmail.com>
2024-03-16 15:42:24 +0800

    refactor: separate window messaging (#282)

--- packages/vite/src/overlay.js
+++ packages/vite/src/overlay.js
@@ -1,6 +1,5 @@
 import vueDevToolsOptions from 'virtual:vue-devtools-options'
-import { Bridge, prepareInjection, setDevToolsClientUrl } from '@vue/devtools-core'
-import { BROADCAST_CHANNEL_NAME } from '@vue/devtools-shared'
+import { initAppSeparateWindow, setDevToolsClientUrl } from '@vue/devtools-core'
 import { addCustomTab, devtools, setDevToolsEnv, toggleComponentInspectorEnabled } from '@vue/devtools-kit'
 
 const overlayDir = `${vueDevToolsOptions.clientHost || ''}${vueDevToolsOptions.base || '/'}@id/virtual:vue-devtools-path:overlay`
@@ -49,34 +48,4 @@ body.appendChild(script)
 // Used in the browser extension
 window.__VUE_DEVTOOLS_VITE_PLUGIN_CLIENT_URL__ = `${window.location.origin}${devtoolsClientUrl}`
 
-// @TODO: refactor separate window channel
-const channel = new BroadcastChannel(BROADCAST_CHANNEL_NAME)
-
-const bridge = new Bridge({
-  tracker(fn) {
-    channel.onmessage = (event) => {
-      if (event.data.source === '__VUE_DEVTOOLS_CLIENT__')
-        fn(event.data.data)
-    }
-  },
-  trigger(data) {
-    channel.postMessage({
-      source: '__VUE_DEVTOOLS_USER_APP__',
-      data,
-    })
-  },
-})
-
-prepareInjection(bridge)
-
-bridge.on('ready', () => {
-  bridge.emit('__VUE_DEVTOOLS_CREATE_CLIENT__')
-  // force sync to make sure that connect the devtools client
-  setTimeout(() => {
-    bridge.emit('syn')
-  }, 200)
-
-  window.addEventListener('beforeunload', (event) => {
-    bridge.emit('disconnect')
-  })
-})
+initAppSeparateWindow()

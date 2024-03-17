f726c6eb890b7b888c281864c9451f9105fc2fe9
arlo <webfansplz@gmail.com>
2024-03-17 14:18:25 +0800

    chore: fix type

--- packages/applet/vite.config.ts
+++ packages/applet/vite.config.ts
@@ -1,6 +1,7 @@
-import { resolve } from 'node:path'
+import { join, resolve } from 'node:path'
 import Vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import fse from 'fs-extra'
 
 const argv = process.argv.slice(2)
 const enableWatch = argv.includes('--watch')
@@ -14,6 +15,17 @@ export default {
   plugins: [
     Unocss(),
     Vue(),
+    {
+      name: 'move-dts',
+      apply: 'build',
+      enforce: 'post',
+      closeBundle() {
+        // copy
+        const targetDir = resolve(__dirname, './dist')
+
+        fse.copySync('./index.d.ts', join(targetDir, './index.d.ts'))
+      },
+    },
   ],
   build: {
     emptyOutDir: !enableWatch,

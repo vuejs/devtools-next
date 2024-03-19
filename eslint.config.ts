8cd6092b88ec21c855918bdda0cb359c3d87dd7a
arlo <webfansplz@gmail.com>
2024-03-20 01:23:19 +0800

    chore: back to ts config for eslint, #256

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,5 @@
 import antfu from '@antfu/eslint-config'
 
-// We temporarily disable `eslint.config.ts` which given by `eslint-ts-patch`
-// Related: https://github.com/antfu/eslint-ts-patch/issues/5
 export default antfu({
   // force enable vue and typescript rules
   vue: true,

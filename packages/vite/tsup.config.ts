ac1a74cabc59764dd7c2eced0a17a341fda67b12
alexzhang1030 <1642114555@qq.com>
2024-04-10 09:26:23 +0800

    fix: force mjs for esm output

--- packages/vite/tsup.config.ts
+++ packages/vite/tsup.config.ts
@@ -9,6 +9,25 @@ export default defineConfig({
   target: 'es2020',
   clean: true,
   format: ['esm', 'cjs'],
+  // Force esbuild to use .mjs extension for ESM output
+  outExtension({ format }) {
+    if (format === 'esm') {
+      return {
+        js: '.mjs',
+        dts: '.d.ts',
+      }
+    }
+    else if (format === 'cjs') {
+      return {
+        js: '.cjs',
+        dts: '.d.cts',
+      }
+    }
+    return {
+      js: '.js',
+      dts: '.d.ts',
+    }
+  },
   // See: https://github.com/evanw/esbuild/issues/1921
   inject: ['./esbuild-shims/cjs-shim.ts'],
   dts: true,

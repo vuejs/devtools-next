a30d34f2628beb2697b679a67fd577be48269dc6
arlo <webfansplz@gmail.com>
2024-04-10 22:58:18 +0800

    chore: update

--- packages/applet/src/modules/components/index.vue
+++ packages/applet/src/modules/components/index.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { Pane, Splitpanes } from 'splitpanes'
-import type { ComponentTreeNode } from '@vue/devtools-kit'
-import { getInspectorTree } from '@vue/devtools-core'
+import type { ComponentTreeNode, InspectorState } from '@vue/devtools-kit'
+import { getInspectorState, getInspectorTree, onInspectorStateUpdated } from '@vue/devtools-core'
 import { parse } from '@vue/devtools-kit'
 import ComponentTree from '~/components/tree/TreeViewer.vue'
 import { createExpandedContext } from '~/composables/toggle-expanded'
 import { createSelectedContext } from '~/composables/select'
+import RootStateViewer from '~/components/state/RootStateViewer.vue'
 
 const inspectorId = 'components'
 const tree = ref<ComponentTreeNode[]>([])
+const activeComponentState = ref<Record<string, InspectorState[]>>({})
+const activeComponentId = ref('')
 
 createExpandedContext()
 createSelectedContext()
 
-const getPiniaInspectorTree = () => {
+function getPiniaInspectorTree() {
   getInspectorTree({ inspectorId, filter: '' }).then((_data) => {
     const data = parse(_data!)
     tree.value = data
+    getComponentState(tree.value?.[0].id)
     console.log('x', tree.value)
   })
 }
+
+function normalizeComponentState(data: { state?: InspectorState[] }) {
+  if (!data || !data?.state)
+    return {}
+  const res = {}
+  data.state.forEach((item) => {
+    if (!res[item.type])
+      res[item.type] = []
+    res[item.type].push(item)
+  })
+  return res
+}
+
+function getComponentState(id: string) {
+  getInspectorState({ inspectorId, nodeId: id }).then((data) => {
+    activeComponentState.value = normalizeComponentState(parse(data!))
+  })
+}
+
+onInspectorStateUpdated((data) => {
+  if (data.inspectorId !== inspectorId)
+    return
+
+  activeComponentState.value = normalizeComponentState({ state: data.state })
+})
+
 getPiniaInspectorTree()
 </script>
 
@@ -34,7 +64,7 @@ getPiniaInspectorTree()
       </Pane>
       <Pane>
         <div h-full select-none overflow-scroll p2 class="no-scrollbar">
-          state
+          <RootStateViewer class="p3" :data="activeComponentState" :node-id="activeComponentId" :inspector-id="inspectorId" />
         </div>
       </Pane>
     </Splitpanes>

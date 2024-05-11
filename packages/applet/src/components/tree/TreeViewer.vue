0376eadc033bbfa444e080a6acd44ce9bee2ddf6
Fan Pei <fanpei920@gmail.com>
2024-05-12 00:04:00 +0900

    feat: Add Vuex applet component

--- packages/applet/src/components/tree/TreeViewer.vue
+++ packages/applet/src/components/tree/TreeViewer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ComponentTreeNode } from '@vue/devtools-kit'
+import type { ComponentTreeNode, InspectorTree } from '@vue/devtools-kit'
 import ToggleExpanded from '~/components/basic/ToggleExpanded.vue'
 import ComponentTreeViewer from '~/components/tree/TreeViewer.vue'
 import NodeTag from '~/components/basic/NodeTag.vue'
@@ -8,7 +8,7 @@ import { useToggleExpanded } from '~/composables/toggle-expanded'
 import { useSelect } from '~/composables/select'
 
 withDefaults(defineProps<{
-  data: ComponentTreeNode[]
+  data: ComponentTreeNode[] | InspectorTree[]
   depth: number
 }>(), {
   depth: 0,
@@ -16,6 +16,10 @@ withDefaults(defineProps<{
 const selectedNodeId = defineModel()
 const { expanded, toggleExpanded } = useToggleExpanded()
 const { select: _select } = useSelect()
+
+function normalizeLabel(item: ComponentTreeNode | InspectorTree) {
+  return ('name' in item && item?.name) || ('label' in item && item.label)
+}
 
 function select(id: string) {
   selectedNodeId.value = id
@@ -46,7 +50,7 @@ function select(id: string) {
       <span v-else pl5 />
       <span font-state-field text-4>
         <span class="text-gray-400 dark:text-gray-600 group-hover:(text-white op50) [.active_&]:(op50 text-white!)">&lt;</span>
-        <span group-hover:text-white class="ws-nowrap [.active_&]:(text-white)">{{ item.name }}</span>
+        <span group-hover:text-white class="ws-nowrap [.active_&]:(text-white)">{{ normalizeLabel(item) }}</span>
         <span class="text-gray-400 dark:text-gray-600 group-hover:(text-white op50) [.active_&]:(op50 text-white!)">&gt;</span>
       </span>
       <NodeTag v-for="(_item, _index) in item.tags" :key="_index" :tag="_item" />

eb9a0fa8c1f027e55061906803b3691d7a765740
David Nahodyl <david@gearboxgo.com>
2024-05-23 12:15:08 -0400

    fixed type String

--- packages/vite/src/vite.ts
+++ packages/vite/src/vite.ts
@@ -53,7 +53,7 @@ export interface VitePluginVueDevToolsOptions {
    *
    * @default code (Visual Studio Code)
    */
-  launchEditor?: 'appcode' | 'atom' | 'atom-beta' | 'brackets' | 'clion' | 'code' | 'code-insiders' | 'codium' | 'emacs' | 'idea' | 'notepad++' | 'pycharm' | 'phpstorm' | 'rubymine' | 'sublime' | 'vim' | 'visualstudio' | 'webstorm' | string
+  launchEditor?: 'appcode' | 'atom' | 'atom-beta' | 'brackets' | 'clion' | 'code' | 'code-insiders' | 'codium' | 'emacs' | 'idea' | 'notepad++' | 'pycharm' | 'phpstorm' | 'rubymine' | 'sublime' | 'vim' | 'visualstudio' | 'webstorm' | String
 
   /**
    * Customize openInEditor host

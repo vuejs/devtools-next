73f57a960361768cd5a5c12d28767c82e74c9737
arlo <webfansplz@gmail.com>
2024-06-05 22:50:06 +0800

    chore: fix types

--- packages/devtools-kit/global.d.ts
+++ packages/devtools-kit/global.d.ts
@@ -1,16 +1,16 @@
-import type { DevToolsContext, DevToolsEnv, DevToolsHook, DevToolsState, RouterInfo } from './src/types'
+import type { DevToolsHook, RouterInfo } from './src/types'
 
 /* eslint-disable vars-on-top, no-var */
 declare global {
   var __VUE_DEVTOOLS_GLOBAL_HOOK__: DevToolsHook
-  var __VUE_DEVTOOLS_GLOBAL_STATE__: DevToolsState
-  var __VUE_DEVTOOLS_CONTEXT__: DevToolsContext
   var __VUE_DEVTOOLS_NEXT_APP_RECORD_INFO__: {
     id: number
     appIds: Set<string>
   }
   var __VUE_DEVTOOLS_ROUTER_INFO__: RouterInfo
-  var __VUE_DEVTOOLS_ENV__: DevToolsEnv
+  var __VUE_DEVTOOLS_ENV__: {
+    vitePluginDetected: boolean
+  }
   var __VUE_DEVTOOLS_COMPONENT_INSPECTOR_ENABLED__: boolean
   var __VUE_DEVTOOLS_VITE_PLUGIN_DETECTED__: boolean
   var __VUE_DEVTOOLS_VITE_PLUGIN_CLIENT_URL__: string

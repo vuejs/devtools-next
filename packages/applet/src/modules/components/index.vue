b28c6d6272132903784cdc309bb24054e9bb3095
Alex <alexzhang1030@foxmail.com>
2024-06-13 17:05:13 +0800

    chore: update

--- packages/applet/src/modules/components/index.vue
+++ packages/applet/src/modules/components/index.vue
@@ -277,7 +277,7 @@ function closeComponentRenderCode() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 style="height: 1.1em; width: 1.1em;color:#00dc82;"
-                class="hover:(op-80)"
+                class="op-80 hover:(op-100)"
                 viewBox="0 0 24 24"
               >
                 <path fill="currentColor" d="M10.611 10.611a1 1 0 0 1 1.11-.208l8.839 3.889a1 1 0 0 1-.14 1.88l-3.338.91l-.91 3.338a1 1 0 0 1-1.88.14l-3.89-8.84a1 1 0 0 1 .209-1.109M17 3a3 3 0 0 1 3 3v3a1 1 0 1 1-2 0V6a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h3a1 1 0 1 1 0 2H6a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3zm-3.73 10.269l1.715 3.9l.318-1.164a1 1 0 0 1 .701-.702l1.165-.318l-3.9-1.716Z" />

222da2e190bb48c362f784e3574be130b0763a8b
Yordan Ramchev <69005114+Yordan-Ramchev@users.noreply.github.com>
2024-06-19 12:15:21 +0300

    docs: update playground link [ci skip] (#398)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
       },
       {
         text: 'Playground',
-        link: 'https://stackblitz.com/github/vuejs/create-vue-templates/tree/main/devtools',
+        link: 'https://stackblitz.com/github/vuejs/create-vue-templates/tree/main/devtools-router-pinia',
       },
     ],
 

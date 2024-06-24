42c7b2528c212bb555ddb9085f5d44894ccf375f
reslear <reslear@gmail.com>
2024-06-24 17:29:00 +0200

    refactor: add title to Header (doc/github)

--- packages/applet/src/components/basic/DevToolsHeader.vue
+++ packages/applet/src/components/basic/DevToolsHeader.vue
@@ -17,10 +17,10 @@ const router = useVirtualRouter()
       </slot>
     </div>
     <div>
-      <a class="pr2" :href="docLink" target="_blank">
+      <a class="pr2" :href="docLink" target="_blank" title="View Documentation">
         <i class="i-clarity:document-line cursor-pointer op70 text-base hover:op100" />
       </a>
-      <a v-if="githubRepoLink" :href="githubRepoLink" target="_blank">
+      <a v-if="githubRepoLink" :href="githubRepoLink" target="_blank" title="Star on GitHub">
         <i class="i-mdi:github cursor-pointer op70 text-base hover:op100" />
       </a>
     </div>

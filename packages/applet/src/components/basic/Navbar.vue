7be89f95d86cf25b9b7bbe6636e66bd850a6061e
reslear <reslear@gmail.com>
2024-06-25 05:53:33 +0200

    improve size for scale compatibility, smaller font size

--- packages/applet/src/components/basic/Navbar.vue
+++ packages/applet/src/components/basic/Navbar.vue
@@ -10,7 +10,7 @@ const router = useVirtualRouter()
     <li
       v-for="(item, index) in routes"
       :key="index"
-      class="h-[40px] flex cursor-pointer items-center justify-center border-b-[2px] border-solid leading-none hover:op100"
+      class="h-10 flex cursor-pointer items-center justify-center border-b-[2px] border-solid text-size-sm leading-none hover:op100"
       :class="currentRoute.path === item.path ? 'op-100 font-medium text-primary-400 border-primary-400' : 'border-transparent op-70'"
       @click="router.push(item.path)"
     >

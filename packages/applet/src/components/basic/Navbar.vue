d6a1fafc3e6464aca24fb18718015eb5f40aa9af
reslear <reslear@gmail.com>
2024-06-25 06:41:52 +0200

    feat(applet): improve custom inspector page nav style (#477)

--- packages/applet/src/components/basic/Navbar.vue
+++ packages/applet/src/components/basic/Navbar.vue
@@ -10,10 +10,8 @@ const router = useVirtualRouter()
     <li
       v-for="(item, index) in routes"
       :key="index"
-      cursor-pointer hover:op100
-      :style="{
-        opacity: currentRoute.path === item.path ? 1 : 0.7,
-      }"
+      class="h-10 flex cursor-pointer items-center justify-center border-b-[2px] border-solid text-size-sm leading-none hover:op100"
+      :class="currentRoute.path === item.path ? 'op-100 font-medium text-primary-400 border-primary-400' : 'border-transparent op-70'"
       @click="router.push(item.path)"
     >
       {{ item.name }}

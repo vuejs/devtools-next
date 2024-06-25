d9256548c391e5ac010de607e5159df8f5748e7c
Alex <alexzhang1030@foxmail.com>
2024-06-25 14:23:34 +0800

    chore: generate on-demand

--- packages/ui/scripts/update-icons.ts
+++ packages/ui/scripts/update-icons.ts
@@ -1,13 +1,31 @@
-import { writeFileSync } from 'node:fs'
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
 import { dirname, resolve } from 'node:path'
 import { icons } from '@iconify-json/ic'
+import { devDependencies } from '../package.json'
+
+const VERSION_COMMENT_RE = /\/\/ Generated from @iconify-json\/ic@(.*)\n/
 
 const __dirname = dirname(new URL(import.meta.url).pathname)
 
-export const icIcons: Record<string, { body: string }> = Object.keys(icons.icons).filter(i => i.startsWith('baseline')).reduce((a, b) => ({ ...a, [b]: icons.icons[b].body }), {})
+const targetPath = resolve(__dirname, '../src/constants/ic-icons.ts')
 
-function update() {
-  writeFileSync(resolve(__dirname, '../src/constants/ic-icons.ts'), `export const icIcons: Record<string, string> = ${JSON.stringify(icIcons, null, 2)}`)
+const targetIcVersion = existsSync(targetPath)
+  ? VERSION_COMMENT_RE.exec(readFileSync(targetPath, 'utf-8'))?.[1] ?? null
+  : null
+
+const icVersion = devDependencies['@iconify-json/ic'].slice(1) /* remove ^ prefix */
+
+if (targetIcVersion === icVersion) {
+  console.log('No need to update ic icons')
+  process.exit(0)
 }
 
-update()
+const withIcVersionComment = (s: string) => `// Generated from @iconify-json/ic@${icVersion}\n\n${s}`
+
+function generateIcIcons() {
+  const icIcons: Record<string, { body: string }> = Object.keys(icons.icons).filter(i => i.startsWith('baseline')).reduce((a, b) => ({ ...a, [b]: icons.icons[b].body }), {})
+  writeFileSync(resolve(__dirname, '../src/constants/ic-icons.ts'), withIcVersionComment(`export const icIcons: Record<string, string> = ${JSON.stringify(icIcons, null, 2)}`))
+  console.log('ic icons updated')
+}
+
+generateIcIcons()
--- packages/ui/src/constants/ic-icons.ts
+++ packages/ui/src/constants/ic-icons.ts
@@ -1,3 +1,5 @@
+// Generated from @iconify-json/ic@1.1.17
+
 export const icIcons: Record<string, string> = {
   'baseline-10k': '<path fill="currentColor" d="M10 10.5h1.5v3H10zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M7.5 15H6v-4.5H4.5V9h3zm5.5-1c0 .55-.45 1-1 1H9.5c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1H12c.55 0 1 .45 1 1zm6.5 1h-1.75L16 12.75V15h-1.5V9H16v2.25L17.75 9h1.75l-2.25 3z"/>',
   'baseline-10mp': '<path fill="currentColor" d="M13.5 7H15v3h-1.5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-7 15.5h-1.5V14h-1v3H8v-3H7v4.5H5.5v-5c0-.55.45-1 1-1H11c.55 0 1 .45 1 1zm3.5 0H14v-6h3.5c.55 0 1 .45 1 1V16c0 .55-.45 1-1 1h-2zM10 5.5v6H8.5V7H7V5.5zm6.5 5c0 .55-.45 1-1 1H13c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1h2.5c.55 0 1 .45 1 1zm-1 3.5H17v1.5h-1.5z"/>',

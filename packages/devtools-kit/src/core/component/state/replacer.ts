ad34863ea8b630a7cd27d178456977200ba53ced
Alex <alexzhang1030@foxmail.com>
2024-07-31 15:20:06 +0800

    chore: update

--- packages/devtools-kit/src/core/component/state/replacer.ts
+++ packages/devtools-kit/src/core/component/state/replacer.ts
@@ -98,7 +98,7 @@ export function stringifyReplacer(key: string | number, _value: any, depth?: num
     else if (val.constructor?.name === 'Store' && '_wrappedGetters' in val) {
       return '[object Store]'
     }
-    else if (ensurePropertyExists(val, 'currentRoute')) {
+    else if (ensurePropertyExists(val, 'currentRoute', true)) {
       return '[object Router]'
     }
     const customDetails = getObjectDetails(val)

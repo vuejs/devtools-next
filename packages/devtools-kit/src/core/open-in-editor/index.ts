05833b65873a41f65ea74ad6f368ff2cf9cee890
Arlo <webfansplz@gmail.com>
2024-08-03 21:26:46 +0800

    feat(extension): support open-in-editor (#543)

--- packages/devtools-kit/src/core/open-in-editor/index.ts
+++ packages/devtools-kit/src/core/open-in-editor/index.ts
@@ -6,6 +6,7 @@ export interface OpenInEditorOptions {
   file?: string
   line?: number
   column?: number
+  host?: string
 }
 
 export function setOpenInEditorBaseUrl(url: string) {
@@ -13,14 +14,22 @@ export function setOpenInEditorBaseUrl(url: string) {
 }
 
 export function openInEditor(options: OpenInEditorOptions = {}) {
-  const { file, baseUrl = window.location.origin, line = 0, column = 0 } = options
+  const { file, host, baseUrl = window.location.origin, line = 0, column = 0 } = options
   if (file) {
-    if (devtoolsState.vitePluginDetected) {
+    if (host === 'chrome-extension') {
+      const fileName = file.replace(/\\/g, '\\\\')
+      // @ts-expect-error skip type check
+      const _baseUrl = window.VUE_DEVTOOLS_CONFIG?.openInEditorHost ?? '/'
+      fetch(`${_baseUrl}__open-in-editor?file=${encodeURI(file)}`).then((response) => {
+        if (!response.ok) {
+          const msg = `Opening component ${fileName} failed`
+          console.log(`%c${msg}`, 'color:red')
+        }
+      })
+    }
+    else if (devtoolsState.vitePluginDetected) {
       const _baseUrl = target.__VUE_DEVTOOLS_OPEN_IN_EDITOR_BASE_URL__ ?? baseUrl
       target.__VUE_INSPECTOR__.openInEditor(_baseUrl, file, line, column)
-    }
-    else {
-      // @TODO: support other
     }
   }
 }

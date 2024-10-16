0573f68ad97cf48d14c472ea8d274188e5f28c41
arlo <webfansplz@gmail.com>
2024-10-16 21:50:59 +0800

    fix(kit): support non `localStorage` env for timeline storage, closes #635

--- packages/devtools-kit/src/core/timeline/storage.ts
+++ packages/devtools-kit/src/core/timeline/storage.ts
@@ -3,14 +3,14 @@ import { isBrowser } from '@vue/devtools-shared'
 const TIMELINE_LAYERS_STATE_STORAGE_ID = '__VUE_DEVTOOLS_KIT_TIMELINE_LAYERS_STATE__'
 
 export function addTimelineLayersStateToStorage(state: Record<string, boolean | string>) {
-  if (!isBrowser) {
+  if (!isBrowser || typeof localStorage === 'undefined') {
     return
   }
   localStorage.setItem(TIMELINE_LAYERS_STATE_STORAGE_ID, JSON.stringify(state))
 }
 
 export function getTimelineLayersStateFromStorage() {
-  if (!isBrowser) {
+  if (!isBrowser || typeof localStorage === 'undefined') {
     return {
       recordingState: false,
       mouseEventEnabled: false,
